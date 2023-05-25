9d90e2d8fbac88b46c29b0c0a85dc43aa7c1671e
Anirudh Suresh <ani@Anirudhs-MacBook-Pro.local>
2023-05-25 11:26:16 -0400

    config file changes

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -2,7 +2,7 @@
 
 # @dev
 # This bash script setups the needed artifacts to use
-# the @aave/deploy-v3 package as source of deployment
+# the @anirudhtx/aave-v3-deploy-pyth package as source of deployment
 # scripts for testing or coverage purposes.
 #
 # A separate  artifacts directory was created 
@@ -29,15 +29,15 @@ mkdir -p temp-artifacts/
 cp -r artifacts/* temp-artifacts/
 
 # Create a symbolic link to reference @aave/periphery-v3 package.json at node_modules
-# required by @aave/deploy-v3 hardhat plugin due hardhat package.json resolution
+# required by @anirudhtx/aave-v3-deploy-pyth hardhat plugin due hardhat package.json resolution
 mkdir -p node_modules/@aave/periphery-v3
 ln -s "$PWD/package.json" node_modules/@aave/periphery-v3/package.json
 
 # Import external @aave/deploy artifacts
 mkdir -p temp-artifacts/deploy/stake
-cp -r node_modules/@aave/deploy-v3/artifacts/contracts/* temp-artifacts/deploy
-cp -r 'node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/stake' temp-artifacts/deploy
-cp -r 'node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/proposals' temp-artifacts/deploy/stake
+cp -r node_modules/@anirudhtx/aave-v3-deploy-pyth/artifacts/contracts/* temp-artifacts/deploy
+cp -r 'node_modules/@anirudhtx/aave-v3-deploy-pyth/artifacts/@aave/safety-module/contracts/stake' temp-artifacts/deploy
+cp -r 'node_modules/@anirudhtx/aave-v3-deploy-pyth/artifacts/@aave/safety-module/contracts/proposals' temp-artifacts/deploy/stake
 
 # Export MARKET_NAME variable to use Aave market as testnet deployment setup
 export MARKET_NAME="Test"
